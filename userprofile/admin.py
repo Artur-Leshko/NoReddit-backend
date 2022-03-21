de6e0832b7d19ee5a5fe6a756bb0af5b06c46db6
Artur Leshko <fire.ghost.01@mail.ru>
2022-03-21 23:18:37 +0300

    register User model in admin site

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,21 @@
+from pyexpat import model
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import AdminUpdateForm, AdminCreationForm
 
-# Register your models here.
+admin.site.unregister(Group)
+
+User = get_user_model()
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = AdminCreationForm
+    add_form = AdminUpdateForm
+
+    list_display = ['username', 'email', 'firstname', 'surname', 'is_staff', 'is_active']
+    list_filter = ['username', 'email', 'is_staff', 'is_active']
+    ordering = ['email']
+    filter_horizontal = ()
+
+admin.site.register(User, CustomUserAdmin)
