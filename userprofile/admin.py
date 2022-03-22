eaee746aa898d2699f3222941a03810fdf4e350a
Artur Leshko <fire.ghost.01@mail.ru>
2022-03-22 14:29:01 +0300

    register UserProfile in admin page

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserProfile
 from .forms import AdminUpdateForm, AdminCreationForm
 
 admin.site.unregister(Group)
@@ -16,9 +17,10 @@ class CustomUserAdmin(BaseUserAdmin):
     form = AdminCreationForm
     add_form = AdminUpdateForm
 
-    list_display = ['username', 'email', 'firstname', 'surname', 'is_staff', 'is_active']
+    list_display = ['username', 'email', 'is_staff', 'is_active']
     list_filter = ['username', 'email', 'is_staff', 'is_active']
     ordering = ['email']
     filter_horizontal = ()
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
