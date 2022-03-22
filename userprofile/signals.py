cf5a3db79df6401473550696351594e6994cad8c
Artur Leshko <fire.ghost.01@mail.ru>
2022-03-22 14:34:58 +0300

    add signal for UserProfile

---
+++ userprofile/signals.py
@@ -0,0 +1,14 @@
+from curses.ascii import US
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import User, UserProfile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    '''
+        Signal for creating UserProfile
+    '''
+
+    if created:
+        UserProfile.objects.create(user=instance, id=instance.id)
+        instance.profile.save()
