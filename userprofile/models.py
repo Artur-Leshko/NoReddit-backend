7d12fee3d563a324a27d9ea5f0233f739c873092
Artur Leshko <fire.ghost.01@mail.ru>
2022-03-22 14:27:40 +0300

    seperate User model on User and UserProfile

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,6 +1,8 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
 
 # def upload_to(instance, filename):
 #     relative_path = instance.url_to_upload.rfind('images/') + len('images/')
@@ -49,20 +51,15 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     '''
-        Model for User profile
+        Model for base User
     '''
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(verbose_name="user name", max_length=200, unique=True)
     email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
-    firstname = models.CharField(verbose_name="user firstname", max_length=200, blank=True)
-    surname = models.CharField(verbose_name="user surname", max_length=200, blank=True)
-    # avatar = models.ForeignKey(Picture, )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -76,6 +73,34 @@ class User(AbstractBaseUser):
 
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+        ordering = ['username']
 
     def __str__(self):
         return f'{self.username}'
+
+
+class UserProfile(models.Model):
+    '''
+        Model for User profile
+    '''
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(verbose_name="user firstname", max_length=200, blank=True)
+    surname = models.CharField(verbose_name="user surname", max_length=200, blank=True)
+    # avatar = models.ForeignKey(Picture, )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        '''
+            Meat class for UserProfile
+        '''
+
+        verbose_name = 'UserProfile'
+        verbose_name_plural = 'UserProfiles'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        if not self.firstname or not self.surname:
+            return f'{self.user.username}'
+        return f'{self.firstname} {self.surname}'
