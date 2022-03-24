03ebc29da356427d45a74fd8acb976e6f4783758
Artur Leshko <fire.ghost.01@mail.ru>
2022-03-24 14:30:04 +0300

    update urls for api userprofile

--- api/userprofile/urls.py
+++ api/userprofile/urls.py
@@ -1,7 +1,7 @@
-from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.UserProfileView.as_view(), name='userprofile'),
+    path('<str:pk>/', views.UserProfilePublicView.as_view(), name="userprofile_public"),
 ]
