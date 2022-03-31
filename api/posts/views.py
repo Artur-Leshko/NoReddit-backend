8ba33cecf7ac3377ec6ea0803c5986ec1ef4d53e
Artur Leshko <fire.ghost.01@mail.ru>
2022-03-31 23:19:24 +0300

    add pagination for posts

--- api/posts/views.py
+++ api/posts/views.py
@@ -1,11 +1,19 @@
 from  datetime import timedelta
 from django.db.models import Q
-from django.contrib.auth import get_user_model
-from rest_framework import permissions, status
+from rest_framework import permissions
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from posts.models import Post
 from .serializers import PostSerializer
+
+class PostPagination(PageNumberPagination):
+    '''
+        Number of posts for pagiantion
+    '''
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class PopularPostsList(generics.ListAPIView):
     '''
@@ -13,4 +21,5 @@ class PopularPostsList(generics.ListAPIView):
     '''
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PostPagination
     queryset = Post.objects.filter(Q(created_at=timedelta(days=3) & Q(votes__gte=50) | Q(created_at=timedelta(days=1))))
