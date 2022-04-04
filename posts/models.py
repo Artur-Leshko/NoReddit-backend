73a5bd8d9e8bb324820b57807fd620d80db1c8f9
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-04 13:45:13 +0300

    add Vote model

--- posts/models.py
+++ posts/models.py
@@ -22,7 +22,36 @@ class Post(models.Model):
 
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
-        ordering = ['created_at']
+        ordering = ['-created_at']
 
     def __str__(self):
         return str(self.title)
+
+
+class Vote(models.Model):
+    '''
+        Model for votes
+    '''
+
+    UPVOTE = "up"
+    DOWNVOTE = "do"
+
+    VOTE_TYPE = [
+        (UPVOTE, 'upvoted'),
+        (DOWNVOTE, 'downvoted')
+    ]
+
+    owner = models.ForeignKey(UserProfile, primary_key=True, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, primary_key=True, on_delete=models.CASCADE)
+    vote_type = models.CharField(max_length=2, choices=VOTE_TYPE, default=UPVOTE)
+    ceated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        '''
+            Meta class for Vote model
+        '''
+
+        verbose_name = "Vote"
+        verbose_name_plural = "Votes"
+        ordering = ['-created_at']
