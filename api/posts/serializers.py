8aaa5c7732336aa695fa11d61bdda2ff936deec4
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-04 20:46:31 +0300

    rewrite serializer for Post model/write serializer for PostAdding

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -1,38 +1,7 @@
-from django.db.models import Q
 from rest_framework import serializers
 
 from api.userprofile.serializers import UserProfileSerializer
 from posts.models import Post, Vote
-
-
-class VoteSerializer(serializers.ModelSerializer):
-    '''
-        Serializer for Vote model
-    '''
-
-    upvotes = serializers.SerializerMethodField()
-    downvotes = serializers.SerializerMethodField()
-
-    class Meta:
-        '''
-            Meta class for VoteSerializer
-        '''
-        model = Vote
-        fields = ['upvotes', 'downvotes']
-
-    def get_upvotes(self):
-        '''
-            returns count of upvotes for post
-        '''
-        return Vote.objects.filter(Q(owner=self.context['request'].user.id) &
-            Q(vote_type='up')).count()
-
-    def get_downvotes(self):
-        '''
-            returns count of downvotes for post
-        '''
-        return Vote.objects.filter(Q(owner=self.context['request'].user.id) &
-            Q(vote_type='down')).count()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -40,14 +9,38 @@ class PostSerializer(serializers.ModelSerializer):
         Serializer for Post model
     '''
 
-    owner = UserProfileSerializer(many=False)
-    owner = serializers.ReadOnlyField()
-
-    votes = VoteSerializer(many=True)
+    owner = UserProfileSerializer()
+    upvotes = serializers.SerializerMethodField()
+    downvotes = serializers.SerializerMethodField()
 
     class Meta:
         '''
             Meta class for PostSerializer
         '''
         model = Post
-        fields = ['owner', 'title', 'main_text', 'votes']
+        fields = ['owner', 'title', 'main_text', 'upvotes', 'downvotes']
+
+    def get_upvotes(self, obj):
+        '''
+            returns count of upvotes for post
+        '''
+        return Vote.objects.filter(post=obj.id, vote_type='up').count()
+
+    def get_downvotes(self, obj):
+        '''
+            returns count of downvotes for post
+        '''
+        return Vote.objects.filter(post=obj.id, vote_type='down').count()
+
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    '''
+        Serializer for creating Post model
+    '''
+
+    class Meta:
+        '''
+            Meta class for CreatePostSerializer
+        '''
+        model = Post
+        fields = ['title', 'main_text']
