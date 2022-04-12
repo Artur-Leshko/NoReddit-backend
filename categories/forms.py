8692a11d9edf487e25510bafde03ce8bd51e179a
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-12 22:07:28 +0300

    implement admin form for category update

--- categories/forms.py
+++ categories/forms.py
@@ -26,3 +26,16 @@ class AdminUpdateCategoryForm(forms.ModelForm):
         Admin form for updateing category
     '''
 
+    class Meta:
+        '''
+            Meta class for AdminUpdateCategoryForm
+        '''
+        model = Category
+        fields = ['name', 'category_image']
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        category_name = cleaned_data.get('name')
+        if Category.objects.filter(name=category_name).exists():
+            self.add_error('name', 'Category with this name already exests!')
