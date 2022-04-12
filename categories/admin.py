059d08053d07ebc2b6d791a582d8c68adbe9ccd3
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-12 22:17:15 +0300

    create custom category admin view

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import Category
+from .forms import AdminCreationCategoryForm, AdminUpdateCategoryForm
+
+class CustomCategoryAdmin(BaseUserAdmin):
+    '''
+        Admin view of Category
+    '''
+
+    form = AdminUpdateCategoryForm
+    add_form = AdminCreationCategoryForm
+
+    list_display = ['id', 'name']
+    list_filter = ['name']
+
+    fieldsets = (
+        ('Data fields', { 'fields': ('name', 'category_image') })
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'category_image')
+        })
+    )
+
+    ordering = ['name']
+    filter_horizontal = ()
+
+admin.site.register(Category, CustomCategoryAdmin)
