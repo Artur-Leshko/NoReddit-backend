0dfdd25149474e2ce66f3e744298295b6f5c60e5
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-12 22:05:11 +0300

    implement admin form for category creation

--- categories/forms.py
+++ categories/forms.py
@@ -6,6 +6,21 @@ class AdminCreationCategoryForm(forms.ModelForm):
         Admin form for creating category
     '''
 
+    class Meta:
+        '''
+            Meta class for AdminCreationCategoryForm
+        '''
+        model = Category
+        fields = ['name', 'category_image']
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        category_name = cleaned_data.get('name')
+        if Category.objects.filter(name=category_name).exists():
+            self.add_error('name', 'Category with this name already exists!')
+
+
 class AdminUpdateCategoryForm(forms.ModelForm):
     '''
         Admin form for updateing category
