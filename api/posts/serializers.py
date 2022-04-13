d4c29884f8a49e22026526fd95dad60ef9633431
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-13 13:23:02 +0300

    update serializers for Posts

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from api.userprofile.serializers import UserProfilePostSerializer
+from api.categories.serializers import CategorySerializer
 from posts.models import Post
 
 
@@ -10,13 +11,14 @@ class PostSerializer(serializers.ModelSerializer):
     '''
 
     owner = UserProfilePostSerializer()
+    categories = serializers.ListSerializer(child=CategorySerializer())
 
     class Meta:
         '''
             Meta class for PostSerializer
         '''
         model = Post
-        fields = ['id', 'owner', 'title', 'main_text', 'upvotes', 'downvotes']
+        fields = ['id', 'owner', 'title', 'main_text', 'upvotes', 'downvotes', 'categories']
         read_only_fields = ['id', 'owner', 'upvotes', 'downvotes']
 
 
@@ -25,9 +27,11 @@ class CreatePostSerializer(serializers.ModelSerializer):
         Serializer for creating Post model
     '''
 
+    categories = serializers.ListSerializer(child=CategorySerializer())
+
     class Meta:
         '''
             Meta class for CreatePostSerializer
         '''
         model = Post
-        fields = ['id', 'title', 'main_text']
+        fields = ['id', 'title', 'main_text', 'categories']
