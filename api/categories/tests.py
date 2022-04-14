b030b3db38282e9cfa447324bffaf0ad278526d2
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-14 14:16:29 +0300

    add test database for categories

--- api/categories/tests.py
+++ api/categories/tests.py
@@ -19,3 +19,26 @@ class CategoriesTests(APITestCase):
         '''
             set ups database for tests
         '''
+
+        self.first_user = User.objects.create_superuser(username='my user',
+            email='abcdf@mail.ru', password='qwe123')
+        self.first_user.save()
+        self.firt_userprofile = UserProfile.objects.create(user=self.first_user,
+            id=self.first_user.id)
+        self.firt_userprofile.save()
+
+        self.second_user = User.objects.create_user(username='my second user',
+            email='beee@mail.ru', password='qwe123')
+        self.second_user.save()
+        self.second_userprofile = UserProfile.objects.create(user=self.second_user,
+            id=self.second_user.id)
+        self.second_userprofile.save()
+
+
+        self.first_user_token = AccessToken.for_user(self.first_user)
+        self.second_user_token = AccessToken.for_user(self.second_user)
+
+        self.first_category = Category.objects.create(title="Tech",
+            main_text="Something about technologies")
+        self.second_category = Category.objects.create(title="Fun",
+            main_text="Some funny jokes here")
