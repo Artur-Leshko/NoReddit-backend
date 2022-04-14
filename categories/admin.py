853c906bdc1ef60a1c6254093b501cb197d2cde5
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-14 23:12:08 +0300

    change admin and admin forms for categories

--- categories/admin.py
+++ categories/admin.py
@@ -1,32 +1,22 @@
 from django.contrib import admin
 
 from .models import Category
-from .forms import AdminCreationCategoryForm, AdminUpdateCategoryForm
+from .forms import AdminCategoryForm
 
 class CustomCategoryAdmin(admin.ModelAdmin):
     '''
         Admin view of Category
     '''
 
-    form = AdminUpdateCategoryForm
-    add_form = AdminCreationCategoryForm
+    form = AdminCategoryForm
 
     list_display = ['id', 'name']
     list_filter = ['created_at']
 
     fieldsets = (
-        ('ID field', { 'fields': ('id',) }),
-        ('Data fields', { 'fields': ('name', 'description', 'category_image') })
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('name', 'description', 'category_image')
-        })
+        ('Data fields', { 'fields': ('name', 'description', 'category_image') }),
     )
 
     ordering = ['name']
-    filter_horizontal = ()
 
 admin.site.register(Category, CustomCategoryAdmin)
