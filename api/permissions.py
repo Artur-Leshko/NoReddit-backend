6e7273068ec1abb64dc8771d328d658db93e68dd
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-19 21:56:14 +0300

    add CommentOwner permission

--- api/permissions.py
+++ api/permissions.py
@@ -7,3 +7,13 @@ class IsPostOwner(BasePermission):
     '''
     def has_object_permission(self, request, view, obj):
         return request.user and request.user.is_authenticated and obj.owner.id == request.user.id
+
+class IsCommentOwner(BasePermission):
+    '''
+        Owner of the comment
+    '''
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner.id == request.user.id
