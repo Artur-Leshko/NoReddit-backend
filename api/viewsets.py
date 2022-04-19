ccebddc21d1c7d2e3fb98f1a515b8392933c72a6
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-19 21:44:52 +0300

    add new viewset using mixins

--- api/viewsets.py
+++ api/viewsets.py
@@ -12,3 +12,15 @@ class MixedPermission:
             return [permission() for permission in self.permission_classes_by_action[self.action]]
         except KeyError:
             return [permission() for permission in self.permission_classes]
+
+
+class CreateRetrieveUpdateDestroyViewset(mixins.CreateModelMixin,
+                                         mixins.RetrieveModelMixin,
+                                         mixins.UpdateModelMixin,
+                                         mixins.DestroyModelMixin,
+                                         MixedPermission,
+                                         viewsets.GenericViewSet):
+    '''
+        viewset for craete/detail/update/delete
+    '''
+    pass
