25f2f99cc8c995c80c9a081905b129861380aafb
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-21 16:11:30 +0300

    tests for /api/v1/comments/:id/ url

--- api/comments/tests.py
+++ api/comments/tests.py
@@ -62,18 +62,51 @@ class CommentsAndCommentVotesTests(APITestCase):
             comment=self.first_user_post_comment, vote_type='up')
         self.first_user_first_comment_vote.save()
         self.second_user_first_comment_vote = CommentVote.objects.create(owner=self.second_userprofile,
-            post=self.first_user_post_comment, vote_type='up')
+            comment=self.first_user_post_comment, vote_type='up')
         self.second_user_first_comment_vote.save()
         self.third_user_first_comment_vote = CommentVote.objects.create(owner=self.third_userprofile,
-            post=self.first_user_post_comment, vote_type='up')
+            comment=self.first_user_post_comment, vote_type='up')
         self.third_user_first_comment_vote.save()
 
         self.first_user_second_comment_vote = CommentVote.objects.create(owner=self.first_userprofile,
-            post=self.second_user_post_comment, vote_type='up')
+            comment=self.second_user_post_comment, vote_type='up')
         self.first_user_second_comment_vote.save()
         self.second_user_second_comment_vote = CommentVote.objects.create(owner=self.second_userprofile,
-            post=self.second_user_post_comment, vote_type='up')
+            comment=self.second_user_post_comment, vote_type='up')
         self.second_user_second_comment_vote.save()
         self.third_user_second_comment_vote = CommentVote.objects.create(owner=self.third_userprofile,
-            post=self.second_user_post_comment, vote_type='down')
+            comment=self.second_user_post_comment, vote_type='down')
         self.third_user_second_comment_vote.save()
+
+    def test_authorized_comment_details(self):
+        '''
+            any authorized user can get comment details
+        '''
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.first_user_token))
+        first_response = self.client.get(reverse('comment-detail',
+            kwargs={"pk": self.first_user_post_comment.id}))
+        second_response = self.client.get(reverse('comment-detail',
+            kwargs={"pk": self.second_user_post_comment.id}))
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.third_user_token))
+        third_response = self.client.get(reverse('comment-detail',
+            kwargs={"pk": self.first_user_post_comment.id}))
+
+        first_serializer = CommentSerializer(self.first_user_post_comment)
+        second_serializer = CommentSerializer(self.second_user_post_comment)
+
+        self.assertEqual(first_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(second_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(third_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(first_response.json(), first_serializer.data)
+        self.assertEqual(second_response.json(), second_serializer.data)
+        self.assertEqual(third_response.json(), first_serializer.data)
+
+    def test_unauthorized_comment_details(self):
+        '''
+            unauthorized user can't get commnet details
+        '''
+        response = self.client.get(reverse('comment-detail',
+            kwargs={"pk": self.first_user_post_comment.id}))
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
