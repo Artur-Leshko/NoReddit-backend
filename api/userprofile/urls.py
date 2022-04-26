12776de4bf5db3650bded162a6227578d0271e9d
Artur Leshko <fire.ghost.01@mail.ru>
2022-04-26 13:06:10 +0300

    add urls for (un-)subscription

--- api/userprofile/urls.py
+++ api/userprofile/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('self/profile/', views.UserProfileView.as_view(), name='userprofile'),
     path('<str:pk>/profile/', views.UserProfilePublicView.as_view(), name="userprofile_public"),
+    path('<str:pk>/subscribe/', views.UserProfileSubscribeView.as_view(), name="userprofile_subscribe"),
+    path('<str:pk>/unsubscribe/', views.UserProfileUnsubscribeView.as_view(), name="userprofile_unsubscrive"),
 ]
