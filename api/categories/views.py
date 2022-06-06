3253ada4fdc04db8770891eadc381c2b5c1af49e
Artur Leshko <fire.ghost.01@mail.ru>
2022-06-06 20:28:51 +0300

    create view that returns posts for category

--- api/categories/views.py
+++ api/categories/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics, permissions, serializers
+from rest_framework.pagination import PageNumberPagination
 
 from api.exeptions import CustomApiException
 from .serializers import CategorySerializer, CategoryDetailSerializer
 from categories.models import Category
+from posts.models import Post
+from api.posts.serializers import PostSerializer
 
 class CategoriesListView(generics.ListAPIView):
     '''
@@ -12,6 +15,14 @@ class CategoriesListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = None
     queryset = Category.objects.all()
+
+class PostPagination(PageNumberPagination):
+    '''
+        Number of posts for pagiantion
+    '''
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 class CategoryRetrieveView(generics.RetrieveAPIView):
     '''
@@ -56,3 +67,22 @@ class CategoryDeleteView(generics.DestroyAPIView):
     '''
     permission_classes = [permissions.IsAdminUser]
     queryset = Category.objects.all()
+
+class CategoryPostsListView(generics.ListAPIView):
+    '''
+        Returns posts of particular category
+    '''
+    serializer_class = PostSerializer
+    pagination_class = PostPagination
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Post.objects.all()
+
+    def get(self, request, pk):
+        '''
+            Returns posts of particular category
+        '''
+        category = Category.objects.get(name=pk)
+        posts = category.posts.all()
+        serializer = PostSerializer(self.paginate_queryset(posts), many=True)
+
+        return self.get_paginated_response(serializer.data)
